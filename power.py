431113d5bcc139a0ca6ceac24937baeabdea8aa2
André "Zan" Rocha <132136080+zanwalker-dev@users.noreply.github.com>
2025-04-08 11:13:51 -0300

    feat: opening the chrome

--- power.py
+++ power.py
@@ -1,5 +1,14 @@
 import pyautogui
 import time
-import pandas
+import pandas 
+
+pyautogui.PAUSE = 0.5
 
 # Access the company website: https://dlp.hashtagtreinamentos.com/python/intensivao/login
+
+# Open Chrome
+pyautogui.keyDown("command")
+pyautogui.press("space")
+pyautogui.keyUp("command")
+pyautogui.write("chrome")
+pyautogui.press("return")
